2c199945928ed2756aea1460fbee95fb565c962a
Simone Bordet <simone.bordet@gmail.com>
2015-04-15 18:43:02 +0200

    Incorporated OpenJDK's jdk7u80-b15 changes.

--- npn-boot/src/main/java/sun/security/ssl/Handshaker.java
+++ npn-boot/src/main/java/sun/security/ssl/Handshaker.java
@@ -641,6 +641,7 @@ abstract class Handshaker {
      */
     ProtocolList getActiveProtocols() {
         if (activeProtocols == null) {
+            boolean enabledSSL20Hello = false;
             ArrayList<ProtocolVersion> protocols = new ArrayList<>(4);
             EnumSet<CryptoPrimitive> cryptoPrimitives =
                 EnumSet.<CryptoPrimitive>of(CryptoPrimitive.KEY_AGREEMENT);
@@ -654,6 +655,12 @@ abstract class Handshaker {
 
                     continue;
                 }
+                // Need not to check the SSL20Hello protocol.
+                if (protocol.v == ProtocolVersion.SSL20Hello.v) {
+                    enabledSSL20Hello = true;
+                    continue;
+                }
+
                 boolean found = false;
                 for (CipherSuite suite : enabledCipherSuites.collection()) {
                     if (suite.isAvailable() && suite.obsoleted > protocol.v &&
@@ -679,6 +686,11 @@ abstract class Handshaker {
                         "No available cipher suite for " + protocol);
                 }
             }
+
+            if (!protocols.isEmpty() && enabledSSL20Hello) {
+                protocols.add(ProtocolVersion.SSL20Hello);
+            }
+
             activeProtocols = new ProtocolList(protocols);
         }
 
